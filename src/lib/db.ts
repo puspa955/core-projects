5726caa54e9d95319c2a79a6aa00c33fabd558a0
puspa <limbup503@gmail.com>
2026-01-03 01:34:09 +0545

    update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,29 +1,15 @@
 // src/lib/db.ts
 import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-import { Pool } from "pg"
 
-const connectionString = process.env.DATABASE_URL
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined")
-}
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  
+  var prisma: PrismaClient | undefined
 }
 
 export const db =
-  globalForPrisma.prisma ??
+  globalThis.prisma ??
   new PrismaClient({
-    adapter,
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db
